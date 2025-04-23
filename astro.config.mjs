1e7e7d8230507348165dd9c0f72583cb3c188dc5
luisnisc <luisnisc30mayo2005@gmail.com>
2025-04-23 18:25:56 +0200

    feat: Refactor code for improved performance and consistency, update API endpoints, and enhance component functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
-
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss({ config: './tailwind.config.js' })],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://noodle.loseardes77.dev',
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
+    plugins: [tailwindcss()],
   },
-  output: 'server',
+  output: "server",
   adapter: vercel(),
-  integrations: [react()],
-  
+  // removed React integration since no React components are used for better performance
 });
