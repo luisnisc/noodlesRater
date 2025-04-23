2a66f5db63aca6804e88e82cb4322a05bd6adff7
luisnisc <luisnisc30mayo2005@gmail.com>
2025-04-23 11:38:01 +0200

    feat: Update Noodles Rater application with improved rating system, new noodle submission form, and Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', 
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'), 
+  ],
+}
